fcf635ec33ff4223d95429decc4d200a0aa2a28a
bin000527 <bin000527@sookmyung.ac.kr>
2023-03-27 17:20:36 +0900

    feat: 미션2 - 1차 기능 구현
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 02. counter/src/App.jsx
+++ 02. counter/src/App.jsx
@@ -1,8 +1,54 @@
-import { useState } from "react";
+import { useState } from 'react';
+import styled from 'styled-components';
+import reactLogo from './assets/react.svg';
 
 const App = () => {
-  console.log('');
+  const [counter, setCounter] = useState(0);
 
-  return ()
-}
+  return (
+    <>
+      <Title>Counter</Title>
+      <Constainer>
+        <Button role="button" onClick={counter > 0 ? () => setCounter(counter - 1) : null}>
+          <img src={reactLogo} alt="decrease" />
+        </Button>
+        <Counter>{counter}</Counter>
+        <Button role="button" onClick={() => setCounter(counter + 1)}>
+          <img src={reactLogo} alt="increase" />
+        </Button>
+      </Constainer>
+    </>
+  );
+};
 export default App;
+
+const Counter = styled.div`
+  width: 50px;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 80px;
+  height: 40px;
+  font-size: 20px;
+  background-color: #efd81d;
+  border-color: #efd81d;
+  outline: none;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const Constainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  margin: 20px auto;
+  font-size: 24px;
+`;
+
+const Title = styled.h1`
+  color: #db5b33;
+  font-weight: 300;
+  text-align: center;
+`;
---
+++ 02. counter/src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans';
+    font-weight: 300;
+  }
+`;
+
+export default GlobalStyle;
