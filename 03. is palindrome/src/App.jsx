7b329df0bb41e1c1edbdd1c43e7ea3975bae72e8
5kdk <tylerk1121@gmail.com>
2023-03-30 13:41:34 +0900

    refactor: m3-Palindrome 컴포넌트 분리
    Co-authored-by: yebin <bin000527@users.noreply.github.com>

--- 03. is palindrome/src/App.jsx
+++ 03. is palindrome/src/App.jsx
@@ -1,84 +1,16 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+// import PalindromeC from './components/PalindromeC';
+import PalindromeUC from './components/PalindromeUC';
 
-const isPalindrome = str => {
-  const replacedStr = str.toLowerCase().replace(/[^ㄱ-힣|a-z|0-9]/g, '');
-
-  return replacedStr && replacedStr === [...replacedStr].reverse().join('');
-};
-
-// Controlled component ver.
-const App = () => {
-  const [inputStr, setInputStr] = useState('');
-  const [submittedStr, setSubmittedStr] = useState('');
-
-  return (
-    <>
-      <Title>is palindrome?</Title>
-      <Checker
-        onSubmit={e => {
-          e.preventDefault();
-
-          setSubmittedStr(inputStr);
-          setInputStr('');
-        }}>
-        <Input
-          value={inputStr}
-          onChange={e => {
-            setInputStr(e.target.value);
-          }}
-        />
-        <Button>Check</Button>
-        {submittedStr && (
-          <Result>{`${submittedStr} is ${isPalindrome(submittedStr) ? '' : 'not'} a palindrome`}</Result>
-        )}
-      </Checker>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Title>is palindrome?</Title>
+    {/* <PalindromeC /> */}
+    <PalindromeUC />
+  </>
+);
 
 export default App;
-
-const Result = styled.p`
-  margin-top: 35px;
-  color: #673ab7;
-  font-weight: 400;
-`;
-
-const Button = styled.button.attrs({ type: 'submit' })`
-  width: 100px;
-  margin-left: 35px;
-  padding: 10px 20px;
-  font-size: 1rem;
-  background-color: #673ab7;
-  border: none;
-  border-radius: 3px;
-  color: #ffffff;
-  cursor: pointer;
-`;
-
-const Input = styled.input.attrs({ type: 'text', name: 'palindrome-input', placeholder: 'Enter a word to check' })`
-  width: 240px;
-  padding: 10px 5px;
-  font-size: 1rem;
-  border: none;
-  border-bottom: 2px solid #d5d5d5;
-  outline: none;
-`;
-
-const Checker = styled.form`
-  width: 450px;
-  height: 200px;
-  background-color: #ffffff;
-  margin: 40px auto;
-  padding: 50px 30px;
-  border-radius: 8px;
-  box-shadow: 0 20px 25px rgba(0, 0, 0, 0.18);
-
-  :focus {
-    border-bottom: 2px solid #673ab7;
-  }
-`;
 
 const Title = styled.h1`
   color: #db5b33;
--- 03. is palindrome/src/main.jsx
+++ 03. is palindrome/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyle from './styles/GlobalStyle';
-// import App from './App';
-import App from './App2';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
