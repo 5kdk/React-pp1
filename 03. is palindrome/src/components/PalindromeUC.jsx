392504b32e5de84016a384147cd89f80594bea51
bin000527 <bin000527@sookmyung.ac.kr>
2023-03-31 20:58:34 +0900

    refactor: m3-styled components 위치 수정 및 컴포넌트 이름 수정
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 03. is palindrome/src/components/PalindromeUC.jsx
+++ 03. is palindrome/src/components/PalindromeUC.jsx
@@ -1,49 +1,18 @@
 import { useState, useRef } from 'react';
 import styled from 'styled-components';
 
-const isPalindrome = str => {
-  const replacedStr = str.toLowerCase().replace(/[^ㄱ-힣|a-z|0-9]/g, '');
+const Form = styled.form`
+  width: 450px;
+  height: 200px;
+  background-color: #ffffff;
+  margin: 40px auto;
+  padding: 50px 30px;
+  border-radius: 8px;
+  box-shadow: 0 20px 25px rgba(0, 0, 0, 0.18);
 
-  return replacedStr && replacedStr === [...replacedStr].reverse().join('');
-};
-
-const PalindromeUC = () => {
-  const [submittedStr, setSubmittedStr] = useState('');
-  const inputRef = useRef();
-
-  return (
-    <Checker
-      onSubmit={e => {
-        e.preventDefault();
-
-        setSubmittedStr(inputRef.current.value);
-        inputRef.current.value = '';
-      }}>
-      <Input ref={inputRef} />
-      <Button>Check</Button>
-      {submittedStr && <Result>{`${submittedStr} is ${isPalindrome(submittedStr) ? '' : 'not'} a palindrome`}</Result>}
-    </Checker>
-  );
-};
-
-export default PalindromeUC;
-
-const Result = styled.p`
-  margin-top: 35px;
-  color: #673ab7;
-  font-weight: 400;
-`;
-
-const Button = styled.button.attrs({ type: 'submit' })`
-  width: 100px;
-  margin-left: 35px;
-  padding: 10px 20px;
-  font-size: 1rem;
-  background-color: #673ab7;
-  border: none;
-  border-radius: 3px;
-  color: #ffffff;
-  cursor: pointer;
+  :focus {
+    border-bottom: 2px solid #673ab7;
+  }
 `;
 
 const Input = styled.input.attrs({
@@ -59,16 +28,47 @@ const Input = styled.input.attrs({
   outline: none;
 `;
 
-const Checker = styled.form`
-  width: 450px;
-  height: 200px;
-  background-color: #ffffff;
-  margin: 40px auto;
-  padding: 50px 30px;
-  border-radius: 8px;
-  box-shadow: 0 20px 25px rgba(0, 0, 0, 0.18);
-
-  :focus {
-    border-bottom: 2px solid #673ab7;
-  }
+const Button = styled.button.attrs({ type: 'submit' })`
+  width: 100px;
+  margin-left: 35px;
+  padding: 10px 20px;
+  font-size: 1rem;
+  background-color: #673ab7;
+  border: none;
+  border-radius: 3px;
+  color: #ffffff;
+  cursor: pointer;
 `;
+
+const Output = styled.p`
+  margin-top: 35px;
+  color: #673ab7;
+  font-weight: 400;
+`;
+
+const isPalindrome = str => {
+  const replacedStr = str.toLowerCase().replace(/[^ㄱ-힣|a-z|0-9]/g, '');
+
+  return replacedStr && replacedStr === [...replacedStr].reverse().join('');
+};
+
+const PalindromeUC = () => {
+  const [submittedStr, setSubmittedStr] = useState('');
+  const inputRef = useRef();
+
+  return (
+    <Form
+      onSubmit={e => {
+        e.preventDefault();
+
+        setSubmittedStr(inputRef.current.value);
+        inputRef.current.value = '';
+      }}>
+      <Input ref={inputRef} />
+      <Button>Check</Button>
+      {submittedStr && <Output>{`${submittedStr} is ${isPalindrome(submittedStr) ? '' : 'not'} a palindrome`}</Output>}
+    </Form>
+  );
+};
+
+export default PalindromeUC;
