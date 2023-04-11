22168571239e27d60052b151a5b7ae5c430570aa
bin000527 <bin000527@sookmyung.ac.kr>
2023-04-11 17:32:15 +0900

    refactor: m14 showMultiple에 따른 함수 관리 방식 변경
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 14. accordion menu/src/components/Accordion.jsx
+++ 14. accordion menu/src/components/Accordion.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import AccordionItem from './AccordionItem';
 
@@ -13,30 +13,23 @@ const AccordionList = styled.div`
   box-shadow: 0 7px 8px -4px rgba(0, 0, 0, 0.2), 0 13px 19px 2px rgba(0, 0, 0, 0.14), 0 5px 24px 4px rgba(0, 0, 0, 0.12);
 `;
 
-const initializer = {
-  multiple(menuList) {
-    return menuList.reduce((ids, { id, isOpen }) => (isOpen ? [...ids, id] : ids), []);
-  },
-  nonMultiple(menuList) {
-    return [menuList.find(menu => menu.isOpen).id];
-  },
-};
+const initialize = (showMultiple, menuList) =>
+  showMultiple
+    ? menuList.reduce((ids, { id, isOpen }) => (isOpen ? [...ids, id] : ids), [])
+    : [menuList.find(menu => menu.isOpen).id];
 
 const Accordion = ({ menuList = [], showMultiple = false }) => {
-  const type = showMultiple ? 'multiple' : 'nonMultiple';
+  const [activeMenuIds, setActiveMenuIds] = useState(initialize(showMultiple, menuList));
 
-  const [activeMenuIds, setActiveMenuIds] = useState(initializer[type](menuList));
-
-  const menuToggler = {
-    multiple(id) {
+  const toggleMenu = useCallback(id => {
+    if (showMultiple) {
       setActiveMenuIds(activeMenuIds =>
         activeMenuIds.includes(id) ? activeMenuIds.filter(_id => _id !== id) : [...activeMenuIds, id]
       );
-    },
-    nonMultiple(id) {
-      setActiveMenuIds(activeMenuIds.includes(id) ? [] : [id]);
-    },
-  };
+    } else {
+      setActiveMenuIds(activeMenuIds => (activeMenuIds.includes(id) ? [] : [id]));
+    }
+  }, []);
 
   return (
     <Container>
@@ -48,7 +41,7 @@ const Accordion = ({ menuList = [], showMultiple = false }) => {
             subMenu={subMenu}
             active={activeMenuIds.includes(id)}
             toggleMenu={() => {
-              menuToggler[type](id);
+              toggleMenu(id);
             }}
             isLastChild={id === menuList.at(-1).id}
           />
