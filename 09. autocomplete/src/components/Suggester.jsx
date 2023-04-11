dff36fd41eeab3d00c6db68bd8d7f0187de41e65
bin000527 <bin000527@sookmyung.ac.kr>
2023-04-11 21:45:08 +0900

    refactor: m9 WIP
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- 09. autocomplete/src/components/Suggester.jsx
+++ 09. autocomplete/src/components/Suggester.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 
 import { debounce } from 'lodash';
@@ -28,16 +28,43 @@ const Suggester = ({ setSelectedCountry }) => {
 
   const userInputRegex = useRef(null);
 
+  useEffect(() => {
+    userInputRegex.current = new RegExp(userInput, 'i');
+  }, [userInput]);
+
+  const debounceRef = useRef(
+    debounce(() => {
+      setFilteredCountryCode(countryCode.filter(([, country]) => userInputRegex.current.test(country)));
+    }, 500)
+  );
+
+  // const debounceFiltering = useCallback(debounce(filterCountryCode, 200), []); // => 강사님 피드백 결과 코드
+
+  /*
+
+  // 처음했던것
+
+  const userInputRegex = useRef(null);
+
   const filterCountryCode = useCallback(() => {
     userInputRegex.current = new RegExp(userInput, 'i');
     setFilteredCountryCode(countryCode.filter(([, country]) => userInputRegex.current.test(country)));
   }, [userInput]);
 
   const debounceFiltering = useMemo(() => debounce(filterCountryCode, 200), [filterCountryCode]);
-  
+
   useEffect(() => {
     debounceFiltering();
   }, [debounceFiltering]);
+   
+  // 이렇게 했었음 아래 onChange에 안넣고 useEffect로
+*/
+  const debounceFiltering = useCallback(
+    debounce(string =>
+      setFilteredCountryCode(countryCode.filter(([, country]) => new RegExp(string, 'i').test(country)))
+    ),
+    []
+  );
 
   return (
     <Container
@@ -45,13 +72,26 @@ const Suggester = ({ setSelectedCountry }) => {
         e.stopPropagation();
       }}>
       <Dropdown>
-        <CountryInput userInput={userInput} onChange={e => setUserInput(e.target.value)} />
+        <CountryInput
+          userInput={userInput}
+          onChange={e => {
+            setUserInput(e.target.value);
+            debounceRef.current?.();
+          }}
+        />
         <CountryList
           userInput={userInput}
           userInputRegex={userInputRegex.current}
           filteredCountryCode={filteredCountryCode}
           setSelectedCountry={setSelectedCountry}
         />
+        {/* <CountryInput userInput={userInput} onChange={e => setUserInput(e.target.value)} />
+        <CountryList
+          userInput={userInput}
+          userInputRegex={userInputRegex.current}
+          filteredCountryCode={filteredCountryCode}
+          setSelectedCountry={setSelectedCountry}
+        /> */}
       </Dropdown>
     </Container>
   );
